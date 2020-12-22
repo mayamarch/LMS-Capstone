c20a0c594f21bf54a9f99dc33c547bd4db2572c2
mayamarch <mayamarchant@gmail.com>
2020-12-22 14:49:10 -0600

    looping through xerrors (not working)

--- LMS-Seal.py
+++ LMS-Seal.py
@@ -1,12 +1,24 @@
 from PTL import *
 
 # data and lines for testing
-test_data = [(1.35, 5), (3.3, 5.3), (4.7, 4.3)]
-test_lines = [[(1, 2), (3.5, 2.25)], [(3.5, 2.25), (4.75, 1)]]
-n = 0.0001
+data = [(1.35, 5), (3.3, 5.3), (4.7, 4.3)]
+lines = [[(1, 2), (3.5, 2.25)], [(3.5, 2.25), (4.75, 1)]]
 
-InitialMove(test_data, test_lines)
+InitialMove(data, lines)
 
-xmax = Max(test_data, 'x')
-print(xmax)
+startlnpt = lines[0][0][0]
+endlnpt = lines[len(lines)-1][1][0]
 
+linerange = endlnpt-startlnpt
+
+n = 0.01
+xerrors = [[0] for i in range(0, linerange/n)]
+print(xerrors)
+
+i = 0
+while i < linerange:
+    move(data, n, 'x')
+    xerrors(i) = GetXErrorAverage(data, lines)
+    i = i+1
+
+print(xerrors)
